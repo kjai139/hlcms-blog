d310544f39f47f36eac3f8db89f81ea4941ca027
simon y <71997308+kjai139@users.noreply.github.com>
2023-04-10 21:47:24 -0400

    logo og image

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import ogImgDefault from 'logo.png'
+import ogImgDefault from '../images/logo.png'
 
 
 const Seo = ({title, description, siteUrl, ogImg, ogTitle, ogDesc}) => {
